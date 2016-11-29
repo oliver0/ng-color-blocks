2427589e4bb992a53537b76d84a9b8bb302869c3
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-29 09:26:51 -0600

    Add option to delete colors

--- server/public/scripts/controllers/settingsController.js
+++ server/public/scripts/controllers/settingsController.js
@@ -3,9 +3,18 @@ colorBlocks.controller('SettingsController', ['$scope', 'DataFactory', function(
   var self = this;
   self.newColor = {};
   self.colors = DataFactory;
+  console.log(DataFactory);
 
   self.addColor = function(){
     self.colors.push(self.newColor.color);
+    console.log(DataFactory);
+  };
+
+  self.deleteColor = function(color){
+    var index = self.colors.indexOf(color);
+    console.log(index);
+    self.colors.splice(index, 1);
+    console.log(self.colors);
   };
 
 }]);
--- server/public/scripts/services/dataFactory.js
+++ server/public/scripts/services/dataFactory.js
@@ -2,21 +2,4 @@ colorBlocks.factory('DataFactory', [function() {
   console.log("data factory running");
   var colors = ['red', 'blue', 'magenta', 'green', 'pink'];
   return colors;
-
 }]);
-
-// app.factory('DataFactory', function() {
-//   console.log('Factory running');
-//
-//   // var currentBudget = 0;
-//   // var message = "hello from the factory";
-//
-//   var publicApi = {
-//     currentBudget: 0,
-//     message: "Boo!",
-//     doThing: function(thing) {
-//       doAThing(thing);
-//     }
-//   };
-//
-//   return publicApi;
