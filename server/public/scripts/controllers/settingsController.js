3de36537479916c6af3f7fb05c4a588536b94824
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-28 19:18:55 -0600

    Add new color to self.colors array

--- server/public/scripts/controllers/settingsController.js
+++ server/public/scripts/controllers/settingsController.js
@@ -1,5 +1,17 @@
-colorBlocks.controller('SettingsController', ['$scope', function($scope) {
+colorBlocks.controller('SettingsController', ['$scope', 'DataFactory', function($scope, DataFactory) {
   console.log("settings controller running");
+  var self = this;
+  self.newColor = {};
+  self.colors = DataFactory;
 
-  
+  self.addColor = function(){
+    self.colors.push(self.newColor.color);
+    console.log(self.colors);
+  };
+
+
+
+
+
+
 }]);
